feb353b1acea49a20530d0d897e8a63e0ea55751
VENU <venumittapalli7@gmail.com>
2018-05-13 08:24:52 +0530

    updated SimpleScoringSevice according to new model(javaDocs and stub implementation)

--- app/src/main/java/com/glarimy/quiz/service/ScoringService.java
+++ app/src/main/java/com/glarimy/quiz/service/ScoringService.java
@@ -5,12 +5,20 @@ import com.glarimy.quiz.model.Score;
 
 public interface ScoringService {
     /**
-     * It returns the score
+     * It returns true or false by checking in the answer with the ticked option
+     * if both matches it returns true else flase
      * Parameters:
      * Answer:get the ticked option from Answer service.
      *
-     * Return:gives the score after evaluating.
+     * Return:return true or flase.
      */
-    public Score evaluate(Answer answer);
+    public boolean evaluate(Answer answer);
+    /**
+     * It returns the score
+     * Parameters:None
+     *
+     * Return:gives the Current score of user .
+     */
+    public Score getCurrentScore();
 
 }
--- app/src/main/java/com/glarimy/quiz/service/SimpleScoringService.java
+++ app/src/main/java/com/glarimy/quiz/service/SimpleScoringService.java
@@ -4,17 +4,16 @@ import com.glarimy.quiz.model.Answer;
 import com.glarimy.quiz.model.Score;
 
 public  class SimpleScoringService implements ScoringService {
-protected int numberOfPoints;
-protected int numberOfAttempts;
+protected Score currentScore;
+
 
     @Override
-    public Score evaluate(Answer answer) {
-        int val=answer.getCorrectOption();
-       int val1=answer.getTickedOption();
-       numberOfPoints=0;
-        Score score=new Score();
-        score.setNumberOfPoints(numberOfPoints);
+    public boolean evaluate(Answer answer) {
+        return false;
+    }
 
-        return score;
+    @Override
+    public Score getCurrentScore() {
+        return null;
     }
 }
