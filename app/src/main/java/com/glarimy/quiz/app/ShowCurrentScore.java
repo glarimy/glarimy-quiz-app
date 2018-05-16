7090340259c7ac9bb9f6c8d29966df5ff93ceca1
VENU <venumittapalli7@gmail.com>
2018-05-16 11:52:26 +0530

    designed page 11 and 12

---
+++ app/src/main/java/com/glarimy/quiz/app/ShowCurrentScore.java
@@ -0,0 +1,14 @@
+package com.glarimy.quiz.app;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowCurrentScore extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_current_score);
+    }
+}
---
+++ app/src/main/java/com/glarimy/quiz/app/ShowErrorMessage.java
@@ -0,0 +1,14 @@
+package com.glarimy.quiz.app;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowErrorMessage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_error_message);
+    }
+}
